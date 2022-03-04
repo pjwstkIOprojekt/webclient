56e292b218cec5c05b8e83fedb3bcbd52d58e05e
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-03-04 14:15:51 +0100

    Rect-Bootstrap and API calls demo
    Added react-bootstrap with some examples and some simple API calls.

--- src/components/Func.tsx
+++ src/components/Func.tsx
@@ -1,7 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { exampleData, exampleGet } from "../apiCalls/exampleCalls";
+import ListGroup from "react-bootstrap/ListGroup";
 
+// Interfaces can use inheritance
+interface exampleType extends exampleData
+{
+    id: number
+}
+
+// Using state in a functional component
 export default function Func()
 {
-    const { value } = useParams();
-    return <h1>{value}</h1>;
+    const [content, setContent] = useState(undefined);
+
+    if (!content)
+    {
+        exampleGet().then(res => res.json()).then(data => {
+            if (data && data.length > 0 && !content) {
+                setContent(data.map((item: exampleType) => (
+                    <ListGroup.Item key={item.id}>
+                        ID Autora: {item.userId}<br />
+                        <b>{item.title}</b> {item.body}
+                    </ListGroup.Item>
+                )));
+            }
+        }, err => console.log(err));
+    }
+
+    return <ListGroup>{content}</ListGroup>;
 }
--- src/helpers/withParams.tsx
+++ src/helpers/withParams.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import { useParams } from "react-router-dom";
 
-export default function withParams(Component: any)
+// You can use this function to pass router path params into a component during export
+export default function withParams(Component: typeof React.Component)
 {
     return (props: JSX.IntrinsicAttributes) => <Component {...props} params={useParams()} />;
 }
