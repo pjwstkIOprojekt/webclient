aaeaaab06de0ee427f766bc24b8b8d6d0d7b1162
Tomasz Le Thanh <s21216@pjwstk.edu.pl>
2022-03-13 17:20:49 +0100

    Added navbar

--- src/apiCalls/exampleCalls.ts
+++ src/apiCalls/exampleCalls.ts
@@ -1,26 +1,23 @@
 // Interfaces in TS are used to define custom object types
-export interface exampleData
-{
-    title: string,
-    body: string,
-    userId: number
+export interface exampleData {
+  title: string;
+  body: string;
+  userId: number;
 }
 
 // API fetch returns a Promise
-export function exampleGet()
-{
-    return fetch("https://jsonplaceholder.typicode.com/posts");
+export function exampleGet() {
+  return fetch("https://jsonplaceholder.typicode.com/posts");
 }
 
 // Fetch can also be used for other requests like POST etc.
 // In TS all data types can be made readonly
-export function examplePost(data: Readonly<exampleData>)
-{
-    return fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        },
-        body: JSON.stringify(data)
-    });
+export function examplePost(data: Readonly<exampleData>) {
+  return fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(data),
+  });
 }
