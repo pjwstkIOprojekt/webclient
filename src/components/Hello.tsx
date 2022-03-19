3dccdc85912be412b3d142eec6fc7c2860508af5
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-03-19 18:10:28 +0100

    Added example form

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,26 @@
-import { Container } from "react-bootstrap";
+import { useDarkMode } from "../hooks/useDarkMode";
+import { Container, Form, FormGroup, Row, Button } from "react-bootstrap";
 
 export default function Hello() {
-  return <Container>Hello</Container>;
+  const dark = useDarkMode();
+
+  return (
+    <Container className="mt-5">
+    <Form>
+        <FormGroup className="mb-3">
+        <Form.Label>Nazwa użytkownika</Form.Label>
+        <Form.Control type="email" />
+      </FormGroup>
+      <FormGroup className="mb-3">
+        <Form.Label>Tekst</Form.Label>
+        <Form.Control type="password" />
+        </FormGroup>
+        <Row className="justify-content-center">
+        <Button className="mt-3 w-25" type="submit" variant={dark ? "warning" : "primary"}>
+          Zaloguj się
+        </Button>
+        </Row>
+      </Form>
+    </Container>
+  );
 }
