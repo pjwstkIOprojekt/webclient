63c7918a5f9eb9ca7010b246543099c8a76f6555
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-04-14 14:39:46 +0200

    Map component quick fix

--- src/components/content/map/MapView.tsx
+++ src/components/content/map/MapView.tsx
@@ -1,10 +1,12 @@
 import { useLoadScript } from "@react-google-maps/api"
 import Map from "./Map"
 
+const lib: ("drawing" | "geometry" | "localContext" | "places" | "visualization")[] = ["places"];
+
 const MapView = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-    libraries: ["places"]
+    libraries: lib
   });
 
   return isLoaded ? <Map /> : <div>Loading...</div>;
--- src/components/fragments/Card.tsx
+++ src/components/fragments/Card.tsx
@@ -5,13 +5,12 @@ import { Card as Inner } from "react-bootstrap";
 interface CardParams {
   className?: string,
   children?: ReactChild[] | ReactChildren[],
-  key?: Key | null,
   style?: CSSProperties
 }
 
 const Card = (props: Readonly<CardParams>) => {
   const darkMode = useDarkMode();
-  return <Inner className={props.className} bg={`custom-${darkMode ? "dark" : "light"}`} key={props.key} style={props.style}>{props.children}</Inner>;
+  return <Inner className={props.className} bg={`custom-${darkMode ? "dark" : "light"}`} style={props.style}>{props.children}</Inner>;
 };
 
 export default Card;
