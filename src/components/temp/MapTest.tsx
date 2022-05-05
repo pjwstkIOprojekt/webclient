f258a911633402957bdbb1db1a89b9e9d415a017
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-05-05 14:03:57 +0200

    Implemented searching for maps

--- src/components/temp/MapTest.tsx
+++ src/components/temp/MapTest.tsx
@@ -1,24 +1,7 @@
-import { useState } from "react";
-import FormControl from "../fragments/FormControl";
-import Button from "../fragments/Button";
 import MapView from "../fragments/MapView";
 
-const Form = (props: any) => {
-  const [txt, setTxt] = useState("");
-
-  const search = () => {
-
-  };
-
-  return <><FormControl value={txt} onChange={e => setTxt(e.target.value)} placeholder="Szukaj" /><Button onClick={search} text="Szukaj" /></>;
-};
-
 const MapTest = () => {
-  const [point, setPoint] = useState<[number, number]>([52.222, 21.015]);
-
-  return (
-    <MapView center={point} initialZoom={12} element={<Form func={(x: any) => setPoint(x)} />} marks={[{coords: point}]} />
-  );
+  return <MapView center={[52.222, 21.015]} initialZoom={12} searchable />;
 };
 
 export default MapTest;
