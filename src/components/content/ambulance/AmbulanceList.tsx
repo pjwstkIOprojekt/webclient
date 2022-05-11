b9d2da6f761f6a1c85059ef64f188bac871560f5
Rafal Osica <s21290@pjwstk@edu.pl>
2022-05-11 15:13:59 +0200

    Updated AmbulanceList

--- src/components/content/ambulance/AmbulanceList.tsx
+++ src/components/content/ambulance/AmbulanceList.tsx
@@ -9,20 +9,16 @@ const AmbulanceList = () => {
   const cols = [
     { name: "#", property: "id" },
     { name: "Rodzaj karetki", property: "kind" },
-    { name: "Przebieg", property: "mileage" },
-    { name: "Pojemność baku", property: "capacity" },
-    { name: "Maksymalna ilość ratowników", property: "maxAmount" },
+    { name: "Lista ratowników", property: "paramedics" },
     { name: "Numer rejestracyjny", property: "registrationNumber" },
     { name: "Czy jest dostępna?", property: "available" , func: (x: boolean) => x ? "Tak" : "Nie"  },
-    { name: "Ratownicy", property: "paramedic" },
-    { name: "Usuń", property: "delete" },
-    { name: "Edytuj", property: "edit" }
+    
     
   ];
 
   const ambulances = [
-    { id: 1, kind:  "Covid", mileage: "10000", capacity:"70", maxAmount: "5", registrationNumber: "WW 40404", available: true, paramedic: <Button text="Przypisz" onClick ={e => navigate("/paramedic")}/>, edit: <Button text="Edytuj" onClick={e => navigate("/ambulance/edit")}/>, delete:  <Button text="Usuń"/>},
-    { id: 2, kind:  "Covid", mileage: "20000", capacity:"80", maxAmount: "6", registrationNumber: "WW 50505", available: false, paramedic: <Button text="Przypisz" onClick ={e => navigate("/paramedic")}/>, edit: <Button text="Edytuj" onClick={e => navigate("/ambulance/edit")}/>, delete: <Button text="Usuń"/>},
+    { id: 1, kind:  "Covid", registrationNumber: "WW 40404", available: true, paramedics: "Jan Nowak  Adam Kowalski" },
+    { id: 2, kind:  "Covid", registrationNumber: "WW 50505", available: false, paramedics: "Jan Nowak  Adam Kowalski"},
 
   ];
 
@@ -30,7 +26,7 @@ const AmbulanceList = () => {
     <Container className="mb-3 justify-content-center text-center">
       <h3>Karetki</h3>
       <Table columns={cols} data={ambulances} />
-      <Button text="Dodaj" onClick={e => navigate("/ambulance/add")} />
+      <Button text="Wróć" onClick={e => navigate("/")} />
     </Container>
   )
 }
