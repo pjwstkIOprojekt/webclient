9fb332344d4842d724624e91c1ed97fa92986807
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-05-11 16:29:48 +0200

    Added table header function

--- src/components/fragments/Map.tsx
+++ src/components/fragments/Map.tsx
@@ -64,12 +64,4 @@ const GeocoderMenu = (props: Readonly<GeocodeParams>) => {
   return null;
 };
 
-const icon = L.icon({
-  iconSize: [25, 41],
-  iconAnchor: [10, 41],
-  popupAnchor: [2, -40],
-  iconUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png"
-});
-
 export default Map;
