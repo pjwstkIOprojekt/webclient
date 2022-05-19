2ae073c9e3d87eea4ee52fe2e8788f82d959f1c1
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-05-19 16:16:56 +0200

    Connected medical info edition

--- src/components/content/userinfo/allergy/EditAllergy.tsx
+++ src/components/content/userinfo/allergy/EditAllergy.tsx
@@ -1,9 +1,10 @@
+import { updateAllergies } from "../../../../apiCalls/medicalInfoCalls";
 import { Container } from "react-bootstrap";
 import AllergyForm from "./AllergyForm";
 
 const EditAllergy = () => {
   const onSubmit = (all: number, src: string, info: string) => {
-
+    updateAllergies(1, src + " - " + info).then(res => res.json()).then(data => console.log(data)).catch(err => console.log(err));
   };
 
   return (
