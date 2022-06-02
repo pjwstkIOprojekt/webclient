6a412ce0157288dcb26591c8bfc7a00240b5e79d
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-06-02 15:18:25 +0200

    Tutorial view rework

--- src/components/content/tutorial/Tutorial.tsx
+++ src/components/content/tutorial/Tutorial.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getTutorialById } from "../../../apiCalls/tutorialCalls";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Row, Col, Alert } from "react-bootstrap";
+import CAlert from "../../fragments/Alert";
 import Rating from "react-rating";
 import CustomCard from "../../fragments/Card";
 
@@ -23,30 +24,58 @@ const Tutorial = () => {
 
   return (
     <Container className="my-5">
-      <h1>Tutorial - {data.name} - {data.tutorialKind}</h1>
-      <Rating emptySymbol="fa fa-star-o fa-2x" fullSymbol="fa fa-star fa-2x" initialRating={calcRating(data.average)} />
-      <CustomCard className="mt-5 mx-3">
-        <Card.Img variant="top" src="/img/thumbnail.jpg" height="360" />
-        <Card.Body>
-          <Card.Title>
-            <h3>Kieszonkowstwo dla idiotów</h3>
-            <ul>
-              <li>
-                <b>Krok 1</b><br />
-                Sprawdź, czy osoba jest nieprzytomna.
-              </li>
-              <li>
-                <b>Krok 2</b><br />
-                Zlokalizuj portfel tej osoby.
-              </li>
-              <li>
-                <b>Krok 3</b><br />
-                Zabierz portfel i po cichu ulotnij się z miejsca zdarzenia.
-              </li>
-            </ul>
-          </Card.Title>
-        </Card.Body>
-      </CustomCard>
+      <h1>Poradnik - {data.name} - {data.tutorialKind}</h1>
+      <Row>
+        <Col>
+          <img src="https://i.ytimg.com/vi/if-2M3K1tqk/maxresdefault.jpg" height="360" width="600" />
+        </Col>
+        <Col>
+          <CustomCard className="mx-3">
+            <Card.Body>
+              <Card.Title>
+                <h3>Kieszonkowstwo dla opornych</h3>
+              </Card.Title>
+              <ul>
+                <li>
+                  <b>Krok 1</b><br />
+                  Sprawdź, czy osoba jest nieprzytomna.
+                </li>
+                <li>
+                  <b>Krok 2</b><br />
+                  Zlokalizuj portfel tej osoby.
+                </li>
+                <li>
+                  <b>Krok 3</b><br />
+                  Zabierz portfel.
+                </li>
+                <li>
+                  <b>Krok 4</b><br />
+                  Po cichu ulotnij się z miejsca zdarzenia.
+                </li>
+                <li>
+                  <b>Krok 5</b><br />
+                  STONKS!
+                </li>
+              </ul>
+            </Card.Body>
+          </CustomCard>
+        </Col>
+      </Row>
+      <CAlert className="my-4">
+        <Alert.Heading>Dodatkowe wskazówki:</Alert.Heading>
+        <ul>
+          <li>
+            <b>Robienie tego w pobliżu komisariatu policji raczej nie jest dobrym pomysłem</b>
+          </li>
+          <li>
+            <b>Nie rób sobie selfie, żeby pochwalić się nową zdobyczą w internecie</b>
+          </li>
+        </ul>
+      </CAlert>
+      <Row className="text-center">
+        <p>Jak pomocny okazał się ten poradnik? Podziel się swoją opinią.</p>
+        <Rating emptySymbol="fa fa-star-o fa-2x" fullSymbol="fa fa-star fa-2x" initialRating={calcRating(data.average)} />
+      </Row>
     </Container>
   );
 };
