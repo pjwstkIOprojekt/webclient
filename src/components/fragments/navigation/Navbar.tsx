73e844d0f15e7b71c3f82b95ea4e98061377a74e
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-06-13 22:40:07 +0200

    Updated navbar hover styles

--- src/components/fragments/navigation/Navbar.tsx
+++ src/components/fragments/navigation/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useDarkModeManager } from "../../../hooks/useDarkMode";
 import { Navbar as Inner, Container, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaClinicMedical, FaMedkit, FaBook, FaMapMarkerAlt, FaUserCircle, FaNotesMedical } from "react-icons/fa";
+import { FaClinicMedical, FaMedkit, FaBook, FaMapMarkerAlt, FaUserCircle, FaNotesMedical, FaUserSecret } from "react-icons/fa";
 import CheckIn from "../../content/staff/CheckIn";
 import { HiOutlineLightBulb } from "react-icons/hi";
 import { IoMdSettings, IoIosPaper } from "react-icons/io";
@@ -22,19 +22,19 @@ const Navbar = () => {
         <Inner.Toggle aria-controls="basic-navbar-nav" />
         <Inner.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" className="d-inline-flex align-items-center">
+            <Nav.Link as={Link} to="/" className={`d-inline-flex align-items-center nav-link-${darkMode.isDark ? "dark" : "light"}`}>
               <FaClinicMedical />
               <span className="px-1">Panel</span>
             </Nav.Link>
-            <Nav.Link as={Link} to="/newreport" className="d-inline-flex align-items-center">
+            <Nav.Link as={Link} to="/newreport" className={`d-inline-flex align-items-center nav-link-${darkMode.isDark ? "dark" : "light"}`}>
               <FaMedkit />
               <span className="px-1">Zgłoszenie</span>
             </Nav.Link>
-            <Nav.Link as={Link} to="/tutorial" className="d-inline-flex align-items-center">
+            <Nav.Link as={Link} to="/tutorial" className={`d-inline-flex align-items-center nav-link-${darkMode.isDark ? "dark" : "light"}`}>
               <FaBook />
               <span className="px-1">Poradniki</span>
             </Nav.Link>
-            <Nav.Link as={Link} to="/map" className="d-inline-flex align-items-center">
+            <Nav.Link as={Link} to="/map" className={`d-inline-flex align-items-center nav-link-${darkMode.isDark ? "dark" : "light"}`}>
               <FaMapMarkerAlt />
               <span className="px-1">Mapa</span>
             </Nav.Link>
@@ -42,7 +42,7 @@ const Navbar = () => {
 
           <Nav>
             <CheckIn />
-            <Nav.Link onClick={darkMode.toggle} className="d-inline-flex align-items-center">
+            <Nav.Link onClick={darkMode.toggle} className={`d-inline-flex align-items-center nav-link-${darkMode.isDark ? "dark" : "light"}`}>
               <HiOutlineLightBulb />
               <span className="px-1">Zmień motyw</span>
             </Nav.Link>
@@ -54,7 +54,7 @@ const Navbar = () => {
                   <span className="px-1">Konto</span>
                 </span>
               }
-              >
+              className={`nav-link-${darkMode.isDark ? "dark" : "light"}`} >
               <NavDropdown.Item as={Link} to="/settings/userdata" className="d-inline-flex align-items-center">
                 <IoMdSettings />
                 <span className="px-1">Ustawienia</span>
@@ -62,6 +62,10 @@ const Navbar = () => {
               <NavDropdown.Item as={Link} to="/settings/medicaldata" className="d-inline-flex align-items-center">
                 <FaNotesMedical />
                 <span className="px-1">Dane medyczne</span>
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/settings/trustedperson" className="d-inline-flex align-items-center">
+                <FaUserSecret />
+                <span className="px-1">Osoba zaufana</span>
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item onClick={() => keycloakClient.authenticated ? keycloakClient.logout() : keycloakClient.login()} className="d-inline-flex align-items-center">
