f465407203a309d13f57b43cfdd95fa9c50e08f9
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-07-09 19:02:09 +0200

    Updated tutorial review system

--- src/components/fragments/util/Rating.tsx
+++ src/components/fragments/util/Rating.tsx
@@ -4,7 +4,9 @@ import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 export interface RatingParams {
   initialValue?: number,
   disabled?: boolean,
-  onChange?: (x: number) => void
+  onChange?: (x: number) => void,
+  onClick?: (x: number) => void,
+  onHover?: (x: number) => void
 }
 
 const Rating = (props: Readonly<RatingParams>) => {
@@ -14,7 +16,7 @@ const Rating = (props: Readonly<RatingParams>) => {
     return normalized % floored >= 0.5 ? floored + 0.5 : floored;
   };
 
-  return <Inner emptySymbol={<AiOutlineStar size={35} />} fullSymbol={<AiFillStar size={35} />} onChange={props.onChange} initialRating={props.initialValue && props.initialValue > 0 ? calcRating(props.initialValue) : 0} readonly={props.disabled} />;
+  return <Inner emptySymbol={<AiOutlineStar size={35} />} fullSymbol={<AiFillStar size={35} />} onClick={props.onClick} onChange={props.onChange} onHover={props.onHover} initialRating={props.initialValue && props.initialValue > 0 ? calcRating(props.initialValue) : 0} readonly={props.disabled} />;
 };
 
 export default Rating;
