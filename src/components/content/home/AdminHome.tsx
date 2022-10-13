9824a68f41a4a698053c9f43b3c703ab76cbd4c9
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-10-13 10:16:30 +0200

    Updated home pages

--- src/components/content/home/AdminHome.tsx
+++ src/components/content/home/AdminHome.tsx
@@ -1,168 +1,57 @@
 import NavButton from "../../fragments/navigation/NavButton";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Card } from "react-bootstrap";
 import CustomCard from "../../fragments/util/Card";
 import PieChart from "../../fragments/charts/PieChart";
-import LineChart, { LineChartSetting } from "../../fragments/charts/LineChart";
 import Table from "../../fragments/util/Table";
 
 const AdminHome = () => {
   const data = [
-    { id: 1, name: "Karetka T", equipment: "", accident: 1 },
-    { id: 2, name: "Karetka R", equipment: "", accident: 1 },
-    { id: 3, name: "Karetka R", equipment: "", accident: 2 },
-    { id: 4, name: "Karetka Covid", equipment: "", accident: 3 }
+    { id: 1, name: "Karetka T", accident: 1 },
+    { id: 2, name: "Karetka R", accident: 1 },
+    { id: 3, name: "Karetka R", accident: 2 },
+    { id: 4, name: "Karetka Covid", accident: 3 }
   ];
 
   const cols = [
     { name: "#", property: "id", sortBy: "id", filterBy: "id" },
     { name: "Karetka", property: "name", sortBy: "name", filterBy: "name" },
-    { name: "Wyposażenie", property: "equipment" },
-    { name: "Zgłoszenie", property: (x: Record<string, any>) => <NavButton to={`${x.accident}`}>Zobacz</NavButton>, sortBy: "accident", filterBy: "accident" }
+    { name: "Wyposażenie", property: (x: Record<string, any>) => <NavButton to={`/admpanel/ambulances/equipment/${x.id}`}>Zobacz</NavButton> },
+    { name: "Zgłoszenie", property: (x: Record<string, any>) => <NavButton to={`/admpanel/reports/${x.accident}`}>Zobacz</NavButton>, sortBy: "accident", filterBy: "accident" }
   ];
 
   const pieDat = [
-    { name: "Ratownicy w akcji", value: 4, fill: "#898934", fillDark: "#898934" },
-    { name: "Ratownicy w gotowości", value: 24, fill: "#893434", fillDark: "#898934" },
-    { name: "Ratownicy nieaktywni", value: 11, fill: "#343489", fillDark: "#898934" }
+    { name: "Ratownicy w akcji", value: 4, fill: "#bbbb00", fillDark: "#5dbf62" },
+    { name: "Ratownicy w gotowości", value: 24, fill: "#5dbf62", fillDark: "#c59812" },
+    { name: "Ratownicy nieaktywni", value: 11, fill: "#343489", fillDark: "#aaaa00" }
   ];
 
-  const lineDat = [
-    {
-      key: "24h",
-      values: {
-        "Wypadki": 0,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 0
-      }
-    },
-    {
-      key: "22h",
-      values: {
-        "Wypadki": 0,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 0
-      }
-    },
-    {
-      key: "20h",
-      values: {
-        "Wypadki": 2,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 0
-      }
-    },
-    {
-      key: "18h",
-      values: {
-        "Wypadki": 1,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 0
-      }
-    },
-    {
-      key: "16h",
-      values: {
-        "Wypadki": 4,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 0
-      }
-    },
-    {
-      key: "14h",
-      values: {
-        "Wypadki": 2,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 1
-      }
-    },
-    {
-      key: "12h",
-      values: {
-        "Wypadki": 0,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 1
-      }
-    },
-    {
-      key: "10h",
-      values: {
-        "Wypadki": 2,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 2
-      }
-    },
-    {
-      key: "8h",
-      values: {
-        "Wypadki": 1,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 4
-      }
-    },
-    {
-      key: "6h",
-      values: {
-        "Wypadki": 3,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 4
-      }
-    },
-    {
-      key: "4h",
-      values: {
-        "Wypadki": 6,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 5
-      }
-    },
-    {
-      key: "2h",
-      values: {
-        "Wypadki": 0,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 2
-      }
-    },
-    {
-      key: "Teraz",
-      values: {
-        "Wypadki": 0,
-        "Ataki terrorystyczne": 0,
-        "Ogniska Covid": 0
-      }
-    },
-  ];
-
-  const lineSet: LineChartSetting[] = [
-    { key: "Wypadki", stroke: "#898934", type: "monotone" },
-    { key: "Ataki terrorystyczne", stroke: "#893434", type: "monotone" },
-    { key: "Ogniska Covid", stroke: "#343489", type: "monotone" }
+  const pieDat2 = [
+    { name: "Wypadki", value: 4, fill: "#bbbb00", fillDark: "#5dbf62" },
+    { name: "Ataki terrorystyczne", value: 0, fill: "#5dbf62", fillDark: "#c59812" },
+    { name: "Ogniska Covid", value: 3, fill: "#343489", fillDark: "#aaaa00" },
+    { name: "Pożary", value: 1, fill: "#000000", fillDark: "#cc4444" }
   ];
 
   return (
     <Container className="mt-5 justify-content-center text-center">
       <h1 className="mb-3">Panel główny</h1>
-      <Row>
-        <Col>
-          <CustomCard>
-            <Card.Header>
-              <Card.Title>Aktualny stan personelu</Card.Title>
-            </Card.Header>
-            <Card.Body>
-              <PieChart width={500} height={600} data={pieDat} label legend tooltip />
-            </Card.Body>
-          </CustomCard>
-        </Col>
-        <Col>
-          <CustomCard>
-            <Card.Header>
-              <Card.Title>Raport z ostatnich 24 godzin</Card.Title>
-            </Card.Header>
-            <Card.Body>
-              <LineChart width={500} height={600} data={lineDat} settings={lineSet} legend tooltip />
-            </Card.Body>
-          </CustomCard>
-        </Col>
+      <Row xs={2}>
+        <CustomCard>
+          <Card.Header>
+            <Card.Title>Aktualny stan personelu</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <PieChart width={500} height={600} data={pieDat} label legend tooltip />
+          </Card.Body>
+        </CustomCard>
+        <CustomCard>
+          <Card.Header>
+            <Card.Title>Raport z ostatnich 24 godzin</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <PieChart width={500} height={600} data={pieDat2} label legend tooltip />
+          </Card.Body>
+        </CustomCard>
       </Row>
       <h2 className="mt-5 mb-3">Karetki aktualnie biorące udział w zgłoszeniach</h2>
       <Table columns={cols} data={data} />
--- src/components/content/home/DispositorHome.tsx
+++ src/components/content/home/DispositorHome.tsx
@@ -1,124 +1,32 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
-import BarChart from "../../fragments/charts/BarChart";
-import CustomCard from "../../fragments/util/Card";
+import { Container, Row } from "react-bootstrap";
+import StatsCircle from "../../fragments/util/StatsCircle";
+import NavButton from "../../fragments/navigation/NavButton";
 import Table from "../../fragments/util/Table";
 
 const DispositorHome = () => {
-  const data = [
-    {
-      key: "Tydzień temu",
-      values: {
-        "Otrzymane zgłoszenia": 4,
-        "Przyjęte zgłoszenia": 2
-      }
-    },
-    {
-      key: "6",
-      values: {
-        "Otrzymane zgłoszenia": 2,
-        "Przyjęte zgłoszenia": 2
-      }
-    },
-    {
-      key: "5",
-      values: {
-        "Otrzymane zgłoszenia": 3,
-        "Przyjęte zgłoszenia": 1
-      }
-    },
-    {
-      key: "4",
-      values: {
-        "Otrzymane zgłoszenia": 0,
-        "Przyjęte zgłoszenia": 0
-      }
-    },
-    {
-      key: "3",
-      values: {
-        "Otrzymane zgłoszenia": 1,
-        "Przyjęte zgłoszenia": 0
-      }
-    },
-    {
-      key: "Przedwczoraj",
-      values: {
-        "Otrzymane zgłoszenia": 3,
-        "Przyjęte zgłoszenia": 2
-      }
-    },
-    {
-      key: "Wczoraj",
-      values: {
-        "Otrzymane zgłoszenia": 3,
-        "Przyjęte zgłoszenia": 3
-      }
-    }
-  ];
-
-  const chartConfig = [
-    {
-      key: "Otrzymane zgłoszenia",
-      stroke: "#4caf50",
-      fill: "#4caf50"
-    },
-    {
-      key: "Przyjęte zgłoszenia",
-      stroke: "#d6a923",
-      fill: "#d6a923"
-    }
-  ];
-
   const tableData = [
-    {
-      date: "2022-07-19 18:00",
-      cause: "Wypadek samochodowy",
-      scale: 2
-    },
-    {
-      date: "2022-07-12 16:53",
-      cause: "Atak terrorystyczny",
-      scale: 4
-    }
+    { date: "2022-07-19 18:00", cause: "Wypadek samochodowy", scale: 2 },
+    { date: "2022-07-12 16:53", cause: "Atak terrorystyczny", scale: 4 }
   ];
 
   const cols = [
-    {
-      name: "Data zgłoszenia",
-      property: "date",
-      sortBy: "date",
-      filterBy: "date"
-    },
-    {
-      name: "Rodzaj zgłoszenia",
-      property: "cause",
-      sortBy: "cause",
-      filterBy: "cause"
-    },
-    {
-      name: "Skala zagrożenia",
-      property: "scale",
-      sortBy: "scale",
-      filterBy: "scale"
-    }
+    { name: "Data zgłoszenia", property: "date", sortBy: "date", filterBy: "date" },
+    { name: "Rodzaj zgłoszenia", property: "cause", sortBy: "cause", filterBy: "cause" },
+    { name: "Skala zagrożenia", property: "scale", sortBy: "scale", filterBy: "scale" }
   ];
 
   return (
-    <Container className="mt-5 justify-content-center text-center">
-      <h1 className="mb-3">Panel główny</h1>
-      <Row>
-        <Col>
-          <BarChart width={750} height={600} settings={chartConfig} data={data} grid tooltip legend />
-        </Col>
-        <Col>
-          <CustomCard>
-            <Card.Header>
-              <h2>Witaj ponownie!</h2>
-            </Card.Header>
-            <Card.Img src="/img/thumbnail.jpg" />
-            <p>Od czasu, kiedy się ostatnio zalogowałeś/aś pojawiły się <b>2 nowe zgłoszenia</b>!</p>
-          </CustomCard>
-        </Col>
+    <Container className="mt-5">
+      <h1 className="mb-3 text-center">Panel główny</h1>
+      <h2 className="mb-3 text-center">Dzisiejsze zgłoszenia</h2>
+      <Row xs={4} className="justify-content-around">
+        <StatsCircle value={4} name="Wypadki" />
+        <StatsCircle value={0} name="Ataki terrorystyczne" />
+        <StatsCircle value={3} name="Ogniska Covid" />
+        <StatsCircle value={1} name="Pożary" />
+      </Row>
+      <Row className="mt-5 justify-content-center">
+        <NavButton to="/map" className="w-25">Otwórz mapę</NavButton>
       </Row>
       <Row className="mt-3">
         <Table columns={cols} data={tableData} />
