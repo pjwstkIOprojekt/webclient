175b8c8010db09266a7df07c1c1855231e723e2b
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-10-26 22:19:54 +0200

    Added pin icons to map filters

--- src/components/content/map/MainMap.tsx
+++ src/components/content/map/MainMap.tsx
@@ -1,4 +1,4 @@
-import { Container, Form } from "react-bootstrap";
+import { Container, Form, Row } from "react-bootstrap";
 import FormCheck from "../../fragments/forms/FormCheck";
 import { useState, useEffect } from "react";
 import L from "leaflet";
@@ -98,20 +98,46 @@ interface MapFormParams {
   setFilters: (x: MarkTypes) => void
 }
 
+const Pin = (props: any) => <img src={props.src} height={25} width={10} alt="Ikonka" />;
+
 const MapForm = (props: Readonly<MapFormParams>) => {
   return (
     <Container>
       <h1 className="mt-3">Mapa</h1>
       <h3 className="mb-3">Filtry (pokaż):</h3>
-      <Form>
-        <FormCheck label="Zdarzenia" value={props.filters & MarkTypes.Incident} onChange={e => props.setFilters(props.filters ^ MarkTypes.Incident)} />
-        <FormCheck label="Ataki terrorystyczne" value={props.filters & MarkTypes.Terrorist} onChange={e => props.setFilters(props.filters ^ MarkTypes.Terrorist)} />
-        <FormCheck label="Pożary" value={props.filters & MarkTypes.Fire} onChange={e => props.setFilters(props.filters ^ MarkTypes.Fire)} />
-        <FormCheck label="Karetki" value={props.filters & MarkTypes.Ambulance} onChange={e => props.setFilters(props.filters ^ MarkTypes.Ambulance)} />
-        <FormCheck label="Szpitale" value={props.filters & MarkTypes.Hospital} onChange={e => props.setFilters(props.filters ^ MarkTypes.Hospital)} />
-        <FormCheck label="Posterunki policji" value={props.filters & MarkTypes.Police} onChange={e => props.setFilters(props.filters ^ MarkTypes.Police)} />
-        <FormCheck label="Alerty" value={props.filters & MarkTypes.Alert} onChange={e => props.setFilters(props.filters ^ MarkTypes.Alert)} />
-        <FormCheck label="Ognisko Covid" value={props.filters & MarkTypes.Covid} onChange={e => props.setFilters(props.filters ^ MarkTypes.Covid)} />
+      <Form className="w-25">
+        <Row xs={2}>
+          <Pin src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Map_pin_icon.svg/1504px-Map_pin_icon.svg.png" />
+          <FormCheck label="Zdarzenia" value={props.filters & MarkTypes.Incident} onChange={e => props.setFilters(props.filters ^ MarkTypes.Incident)} />
+        </Row>
+        <Row xs={2}>
+          <Pin src="https://freesvg.org/img/Map-Warning-Icon.png" />
+          <FormCheck label="Ataki terrorystyczne" value={props.filters & MarkTypes.Terrorist} onChange={e => props.setFilters(props.filters ^ MarkTypes.Terrorist)} />
+        </Row>
+        <Row xs={2}>
+          <Pin src="https://cdn3.iconfinder.com/data/icons/map-markers-2-1/512/danger-512.png" />
+          <FormCheck label="Pożary" value={props.filters & MarkTypes.Fire} onChange={e => props.setFilters(props.filters ^ MarkTypes.Fire)} />
+        </Row>
+        <Row xs={2}>
+          <Pin src="https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png" />
+          <FormCheck label="Karetki" value={props.filters & MarkTypes.Ambulance} onChange={e => props.setFilters(props.filters ^ MarkTypes.Ambulance)} />
+        </Row>
+        <Row xs={2}>
+          <Pin src="https://cdn-icons-png.flaticon.com/512/8/8115.png?w=826&t=st=1666208765~exp=1666209365~hmac=e2449742142cbe00032697d49c7d236b48769e71bb872561825ba786688ddfc3" />
+          <FormCheck label="Szpitale" value={props.filters & MarkTypes.Hospital} onChange={e => props.setFilters(props.filters ^ MarkTypes.Hospital)} />
+        </Row>
+        <Row xs={2}>
+          <Pin src="https://cdn3.iconfinder.com/data/icons/streamline-icon-set-free-pack/48/Streamline-20-512.png" />
+          <FormCheck label="Posterunki policji" value={props.filters & MarkTypes.Police} onChange={e => props.setFilters(props.filters ^ MarkTypes.Police)} />
+        </Row>
+        <Row xs={2}>
+          <Pin src="https://cdn2.iconfinder.com/data/icons/danger-problems-2/512/xxx013-512.png" />
+          <FormCheck label="Alerty" value={props.filters & MarkTypes.Alert} onChange={e => props.setFilters(props.filters ^ MarkTypes.Alert)} />
+        </Row>
+        <Row xs={2}>
+          <Pin src="https://img.icons8.com/cotton/344/coronavirus-hospital-map-pin--v2.png" />
+          <FormCheck label="Ognisko Covid" value={props.filters & MarkTypes.Covid} onChange={e => props.setFilters(props.filters ^ MarkTypes.Covid)} />
+        </Row>
       </Form>
     </Container>
   );
@@ -121,7 +147,7 @@ const MainMap = () => {
   //const [accidents, setAccidents] = useState([]);
   //const [ambulances, setAmbulances] = useState([]);
   //const [facilities, setFacilities] = useState([]);
-  const [filters, setFilters] = useState(7);
+  const [filters, setFilters] = useState(255);
   const [update, setUpdate] = useState(false);
 
   useEffect(() => {
