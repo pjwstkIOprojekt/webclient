f67a7430518e0326cfb847269671ac1c7c2334b1
Adam Szerszenowicz <s20967@pjwstk.edu.pl>
2022-12-07 14:14:07 +0100

    Updated new views

--- src/components/content/ambulance/AmbulanceEquipment.tsx
+++ src/components/content/ambulance/AmbulanceEquipment.tsx
@@ -1,15 +1,22 @@
+import { ItemResponse, getItems, EquipmentResponse } from "../../../api/itemCalls";
 import { useState, useEffect } from "react";
-import { EquipmentResponse, getItems } from "../../../api/itemCalls";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { licensePlateError } from "../sharedStrings";
 import { getItems as getAmbulanceItems } from "../../../api/ambulanceCalls";
+import Enum from "../../fragments/values/Enum";
+import { ItemType } from "../../../api/enumCalls";
 import { Container, Row, Col } from "react-bootstrap";
 import NavButton from "../../fragments/navigation/NavButton";
 import Table from "../../fragments/util/Table";
 
+interface ItemData extends ItemResponse {
+  amount: number,
+  unit: string
+}
+
 const AmbulanceEquipment = () => {
-  const [items] = useState<EquipmentResponse[]>([]);
+  const [items, setItems] = useState<ItemData[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const { ambulanceId } = useParams();
   const { t } = useTranslation();
@@ -23,10 +30,19 @@ const AmbulanceEquipment = () => {
     const itemsReq = getItems().then(res => res.json());
     const ambReq = getAmbulanceItems(ambulanceId).then(res => res.json());
 
-    Promise.all([itemsReq, ambReq]).then((data: [any, any]) => {
+    Promise.all([itemsReq, ambReq]).then((data: [ItemResponse[], EquipmentResponse[]]) => {
       if (data) {
-        console.log(data[0]);
-        console.log(data[1]);
+        const ambulanceItems = data[1].map(i => i.item.itemId);
+
+        setItems([...data[1].map(i => ({
+          ...i.item,
+          amount: i.itemData.count,
+          unit: i.itemData.unit
+        })), ...data[0].filter(i => !ambulanceItems.includes(i.itemId)).map(i => ({
+          ...i,
+          amount: 0,
+          unit: ""
+        }))]);
       }
 
       setIsLoading(false);
@@ -36,13 +52,20 @@ const AmbulanceEquipment = () => {
     });
   }, [ambulanceId]);
 
+  const nameField = "name";
+  const typeField = "itemType";
+  const descField = "description";
+
   const cols = [
-    { name: "#", property: "id", filterBy: "id", sortBy: "id" }
+    { name: t("Report.Assigned"), property: (x: Readonly<ItemData>) => <></> },
+    { name: t("Equipment.Name"), property: nameField, filterBy: nameField, sortBy: nameField },
+    { name: t("Equipment.Type"), property: (x: Readonly<ItemData>) => <Enum enum={ItemType} value={x.itemType} />, filterBy: typeField, sortBy: typeField },
+    { name: t("Equipment.Description"), property: descField, filterBy: descField, sortBy: descField }
   ];
 
   return (
-    <Container className="my-3 justify-content-center">
-      <h3 className="text-center">{t("Equipment.Ambulance")}</h3>
+    <Container className="my-3 justify-content-center text-center">
+      <h3>{t("Equipment.Ambulance")}</h3>
       <Row className="my-3 justify-content-end">
         <Col />
         <Col md="auto">
