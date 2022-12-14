7d926346f6024984f327bdd1b425d983b83279c2
Rafal Osica <s21290@pjwstk.edu.pl>
2022-12-14 15:31:12 +0100

    Added icons

--- src/components/content/map/MapIcons.ts
+++ src/components/content/map/MapIcons.ts
@@ -8,15 +8,15 @@ const createIcon = (url: string) => L.icon({
 });
 
 export const ambulanceIcon = createIcon("/img/map/ambulance_icon.svg");
-export const accidentIcon = createIcon("/img/map/hospital_icon.svg");
-export const floodIcon = createIcon("/img/map/hospital_icon.svg");
-export const fireIcon = createIcon("/img/map/hospital_icon.svg");
-export const placeholderIcon = createIcon("/img/map/hospital_icon.svg");
-export const heartIcon = createIcon("/img/map/hospital_icon.svg");
-export const suicideIcon = createIcon("/img/map/hospital_icon.svg");
-export const covidIcon = createIcon("/img/map/hospital_icon.svg");
+export const accidentIcon = createIcon("/img/map/accident_icon.svg");
+export const floodIcon = createIcon("/img/map/flood_icon.svg");
+export const fireIcon = createIcon("/img/map/fire_icon.svg");
+export const placeholderIcon = createIcon("/img/map/placeholder_icon.svg");
+export const heartIcon = createIcon("/img/map/heart_icon.svg");
+export const suicideIcon = createIcon("/img/map/suicide_icon.svg");
+export const covidIcon = createIcon("/img/map/covid_icon.svg");
 export const hospitalIcon = createIcon("/img/map/hospital_icon.svg");
 export const policeIcon = createIcon("/img/map/police_icon.svg");
 export const fireUnitIcon = createIcon("/img/map/fire_department_icon.svg");
-export const guardIcon = createIcon("/img/map/hospital_icon.svg");
-export const aedIcon = createIcon("/img/map/hospital_icon.svg");
+export const guardIcon = createIcon("/img/map/guard_icon.svg");
+export const aedIcon = createIcon("/img/map/aed_icon.svg");
